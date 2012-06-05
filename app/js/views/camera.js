7fd54b0b0cb074ad2e9f5cefdefe8a01fe255da8
Jonathan Forbes <elektrifi@gmail.com>
2012-06-05 23:05:59 +0100

    Debug

--- app/js/views/camera.js
+++ app/js/views/camera.js
@@ -1,15 +1,15 @@
 
 $(document).ready(function(){
 
-  video = document.getElementById("live")
-    var ctx;
-    // use the chrome specific GetUserMedia function
-    navigator.webkitGetUserMedia("video",
-            function(stream) {
-                video.src = webkitURL.createObjectURL(stream);
-            },
-            function(err) {
-                console.log("Unable to get video stream!")
-            }
-    )
+    video = document.getElementById("live")
+      var ctx;
+      // use the chrome specific GetUserMedia function
+      navigator.webkitGetUserMedia("video",
+              function(stream) {
+                  video.src = webkitURL.createObjectURL(stream);
+              },
+              function(err) {
+                  console.log("Unable to get video stream!")
+              }
+      )
 })
