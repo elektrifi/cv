e7303f4a25da34feb13b74b268e6d5c0716449ab
Jonathan Forbes <elektrifi@gmail.com>
2012-06-05 17:59:51 +0100

    Changes to email settings (back to GMail)

--- app/modules/email-settings.js
+++ app/modules/email-settings.js
@@ -1,9 +1,9 @@
 
 module.exports = {
 	
-	host		: 'smtp.1and1.co.uk',
-	user 		: 'jonathan.forbes@elektrifi.com',
-	password 	: 'S0ucd_wywh#2',
-	sender		: 'Jonathan Forbes <jonathan.forbes@elektrifi.com>'
+	host		: 'smtp.gmail.com',
+	user 		: 'elektrifi@gmail.com',
+	password 	: 'jrf1abd01',
+	sender		: 'Jonathan Forbes <elektrifi@gmail.com>'
 	
 }
