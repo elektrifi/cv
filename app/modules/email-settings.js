ed6516a014ac9df257a22d9fc5b838fe0602b93a
Jonathan Forbes <elektrifi@gmail.com>
2012-06-05 18:51:38 +0100

    New AWS SES credentials applied

--- app/modules/email-settings.js
+++ app/modules/email-settings.js
@@ -1,9 +1,9 @@
 
 module.exports = {
 	
-	host		: 'smtp.gmail.com',
-	user 		: 'elektrifi@gmail.com',
-	password 	: 'notonyournelly',
+	host		: 'email-smtp.us-east-1.amazonaws.com',
+	user 		: 'AKIAJFDY4HPTZLEY4DBQ',
+	password 	: 'Ai1TyO0jOilC5apRpl7btCD1FX21cq0S4tpdXndFPXDR',
 	sender		: 'Jonathan Forbes <elektrifi@gmail.com>'
 	
 }
