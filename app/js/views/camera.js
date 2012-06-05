9f300c8b553f1032c0daeaed9963c46ffc629026
Jonathan Forbes <elektrifi@gmail.com>
2012-06-05 20:00:07 +0100

    Added camera view

---
+++ app/js/views/camera.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  video = document.getElementById("live")
+    var ctx;
+    // use the chrome specific GetUserMedia function
+    navigator.webkitGetUserMedia("video",
+            function(stream) {
+                video.src = webkitURL.createObjectURL(stream);
+            },
+            function(err) {
+                console.log("Unable to get video stream!")
+            }
+    )
+})
