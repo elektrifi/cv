e09fb508b44c356de0af8e81a6333af89aa95e9d
Jonathan Forbes <elektrifi@gmail.com>
2012-06-05 21:30:32 +0100

    Debugging and settng new session secret

--- app/core/config.js
+++ app/core/config.js
@@ -7,7 +7,7 @@ module.exports = function(app, exp) {
 		app.set('view options', { pretty: true });
 		app.use(exp.bodyParser());
 		app.use(exp.cookieParser());
-		app.use(exp.session({ secret: 'super-duper-secret-secret' }));
+		app.use(exp.session({ secret: 'Tm0FvR~1cEg7Ki2xUBK2lNc5agf_3il-!<4kMWFATguCTE@:Wi310Y47Nr' }));
 		app.use(exp.methodOverride());
 		app.use(require('stylus').middleware({ src: app.root + '/app' }));
 		app.use(exp.static(app.root + '/app'));
