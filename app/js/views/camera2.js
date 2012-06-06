a821803ccd684d33e7aeb0d990154d1493ee280f
Jonathan Forbes <elektrifi@gmail.com>
2012-06-06 15:06:28 +0100

    Debugging camera2 components

---
+++ app/js/views/camera2.js
@@ -0,0 +1,12 @@
+    var video = $("#live").get()[0];
+    var canvas = $("#canvas");
+    var ctx = canvas.get()[0].getContext('2d');
+    navigator.webkitGetUserMedia("video",
+      function(stream) { video.src = webkitURL.createObjectURL(stream); },
+      function(err) { console.log("Unable to get video stream!") }
+    );
+    timer = setInterval(
+      function () {
+        ctx.drawImage(video, 0, 0, 320, 240);
+      }, 250
+    );
