3f1869530a3077b6eaae827dcdc976516334b5cc
Jonathan Forbes <elektrifi@gmail.com>
2012-06-27 15:52:20 +0100

    Added jQuery-UI theme

--- app/js/views/ar.js
+++ app/js/views/ar.js
@@ -1,3 +1,18 @@
+	// increase the default animation speed to exaggerate the effect
+	$.fx.speeds._default = 1000;
+	$(function() {
+		$( "#dialog" ).dialog({
+			autoOpen: false,
+			show: "blind",
+			hide: "explode"
+		});
+
+		$( "#opener" ).click(function() {
+			$( "#dialog" ).dialog( "open" );
+			return false;
+		});
+	});
+
     var video = $("#live").get()[0];
     navigator.webkitGetUserMedia("video",
       function(stream) { video.src = webkitURL.createObjectURL(stream); },
